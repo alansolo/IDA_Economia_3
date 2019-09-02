251af16fc55ae577e33f2d90c848f90d4d314d4a
Alan <Alan@DESKTOP-M949GPQ>
2019-09-02 01:02:20 -0500

    Agregar Log

---
+++ Datos/Log/BDLog.cs
@@ -0,0 +1,101 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Log
+{
+    public class BDLog:BD
+    {
+        public object ObtenerLog(string spName, List<Parametro> listParametro)
+        {
+            object Resultado = new object();
+            List<SqlParameter> listParametrosSQL = new List<SqlParameter>();
+
+            try
+            {
+                listParametrosSQL = listParametro.Select(n => new SqlParameter
+                {
+                    ParameterName = "@" + n.Nombre,
+                    Value = n.Valor
+                }).ToList();
+
+                Resultado = ExecuteScalar(spName, listParametrosSQL);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Resultado;
+        }
+
+        public object ObtenerLogDetalle(string spName, List<Parametro> listParametro)
+        {
+            object Resultado = new object();
+            List<SqlParameter> listParametrosSQL = new List<SqlParameter>();
+
+            try
+            {
+                listParametrosSQL = listParametro.Select(n => new SqlParameter
+                {
+                    ParameterName = "@" + n.Nombre,
+                    Value = n.Valor
+                }).ToList();
+
+                Resultado = ExecuteScalar(spName, listParametrosSQL);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Resultado;
+        }
+
+        public object InsertLog(string spName, List<Parametro> listParametro, string spNameDetalle, List<Parametro> listParametroDetalle)
+        {
+            object Resultado = new object();
+            List<SqlParameter> listParametrosSQL = new List<SqlParameter>();
+
+            try
+            {
+                listParametrosSQL = listParametro.Select(n => new SqlParameter
+                {
+                    ParameterName = "@" + n.Nombre,
+                    Value = n.Valor
+                }).ToList();
+
+                Resultado = ExecuteScalar(spName, listParametrosSQL);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Resultado;
+        }
+
+        public object InsertLogDetalle(string spName, List<Parametro> listParametro)
+        {
+            object Resultado = new object();
+            List<SqlParameter> listParametrosSQL = new List<SqlParameter>();
+
+            try
+            {
+                listParametrosSQL = listParametro.Select(n => new SqlParameter
+                {
+                    ParameterName = "@" + n.Nombre,
+                    Value = n.Valor
+                }).ToList();
+
+                Resultado = ExecuteScalar(spName, listParametrosSQL);
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Resultado;
+        }
+    }
+}
