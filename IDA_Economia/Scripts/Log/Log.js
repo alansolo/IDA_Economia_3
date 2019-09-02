e75f12bec374ed7e3338bf12b0de99651f86bd12
Alan <Alan@DESKTOP-M949GPQ>
2019-09-02 00:59:15 -0500

    Agregar pantalla de Log y Usuario

---
+++ IDA_Economia/Scripts/Log/Log.js
@@ -0,0 +1,116 @@
+﻿var app = angular.module("MyApp", []);
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('main').className += 'loaded';
+});
+
+//funcion inicial para agregar las empresas
+app.controller("MyController", function ($scope, $http, $window) {
+
+    var urlPathSystem = "";
+
+    var myDate = new Date();
+    var dd = myDate.getDate();
+
+    var mm = myDate.getMonth() + 1;
+    var yyyy = myDate.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+
+    myDate = dd + '/' + mm + '/' + yyyy;
+
+    //Inicializar fecha
+    var date_input = $(".fecha");
+    date_input.datepicker({
+        format: 'dd/mm/yyyy',
+        todayHighlight: true,
+        autoclose: true,
+        language: "es",
+        setDate: myDate
+    });
+
+    $scope.FechaInicio = myDate;
+    $scope.FechaFinal = myDate;
+
+    $scope.ObtenerLog = function () {
+        $('#myModalLoader').modal('show');
+
+        $.ajax({
+            type: "POST",
+            url: urlPathSystem + "/MercadoCapital/ObtenerEstadistico",
+            data: JSON.stringify(
+                {
+                    'strFechaInicio': $scope.FechaInicio,
+                    'strFechaFinal': $scope.FechaFinal
+                }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (datos) {
+
+                $scope.ListGraficoLinea = datos.ListaCurvaVarianza;
+
+                $scope.ListEncabezadoEmpresa = datos.ListaEncabezadoEmpresa;
+
+                $scope.ListDatos = datos.ListaDatos;
+
+                $scope.ListEmpresa = datos.ListaEmpresa;
+
+                MessageSuccess("Log", "Se obtuvo correctamente la informacion.");
+
+                $scope.$apply();
+
+                $("#myModalLoader").modal('hide');
+
+            },
+            error: function (error) {
+
+                $("#myModalLoader").modal('hide');
+            }
+        });
+    };
+
+    function MessageInfo(titulo, message) {
+        $.notify({
+            // options
+            icon: 'fa fa-info-circle fa-lg',
+            title: "<span class='title-notify'><strong>" + titulo + "</strong></span><br/>",
+            message: "<span class='message-notify'>" + message + "</span><br/>"
+        }, {
+            // settings
+            type: 'info',
+            delay: 8000
+        });
+    }
+
+    function MessageSuccess(titulo, message) {
+        $.notify({
+            // options
+            icon: 'fa fa-check-circle fa-lg',
+            title: "<span class='title-notify'><strong>" + titulo + "</strong></span><br/>",
+            message: "<span class='message-notify'>" + message + "</span><br/>"
+        }, {
+            // settings
+            type: 'success',
+            delay: 8000
+        });
+    }
+
+    function MessageDanger(titulo, message) {
+        $.notify({
+            // options
+            icon: 'fa fa-window-close fa-lg',
+            title: "<span class='title-notify'><strong>" + titulo + "</strong></span><br/>",
+            message: "<span class='message-notify'>" + message + "</span><br/>"
+        }, {
+            // settings
+            type: 'danger',
+            delay: 8000
+        });
+    }
+
+});
---
+++ Negocio/Log/Log.cs
@@ -0,0 +1,108 @@
+﻿using Datos.Log;
+using Entidades;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Log
+{
+    public class Log
+    {
+        public List<Log> ObtenerLog(List<Parametro> listParametro)
+        {
+            object Resultado = new object();
+            const string spName = "ObtenerLog";
+            List<Log> ListLog = new List<Log>();
+            BDLog bdLog = new BDLog();
+
+
+            try
+            {
+                Resultado = bdLog.ObtenerLog(spName, listParametro);
+
+                Resultado = "[" + Resultado + "]";
+
+                var jsonLog = JsonConvert.DeserializeObject<Log[]>(Resultado.ToString());
+                ListLog = jsonLog.ToList();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return ListLog;
+        }
+        public List<LogDetalle> ObtenerLogDetalle(List<Parametro> listParametro)
+        {
+            object Resultado = new object();
+            const string spName = "ObtenerLogDetalle";
+            List<LogDetalle> ListLog = new List<LogDetalle>();
+            BDLog bdLog = new BDLog();
+
+
+            try
+            {
+                Resultado = bdLog.ObtenerLogDetalle(spName, listParametro);
+
+                Resultado = "[" + Resultado + "]";
+
+                var jsonLogDetalle = JsonConvert.DeserializeObject<LogDetalle[]>(Resultado.ToString());
+                ListLog = jsonLogDetalle.ToList();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return ListLog;
+        }
+        public List<Log> InsertLog(List<Parametro> listParametro, List<Parametro> listParametroDetalle)
+        {
+            object Resultado = new object();
+            const string spName = "ObtenerLog";
+            const string spNameDetalle = "ObtenerLogDetalle";
+            List<Log> ListLog = new List<Log>();
+            BDLog bdLog = new BDLog();
+
+
+            try
+            {
+                Resultado = bdLog.InsertLog(spName, listParametro, spNameDetalle, listParametroDetalle);
+
+                Resultado = "[" + Resultado + "]";
+
+                var jsonLog = JsonConvert.DeserializeObject<Log[]>(Resultado.ToString());
+                ListLog = jsonLog.ToList();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return ListLog;
+        }
+        public List<LogDetalle> InsertLogDetalle(List<Parametro> listParametro)
+        {
+            object Resultado = new object();
+            const string spName = "ObtenerLogDetalle";
+            List<LogDetalle> ListLog = new List<LogDetalle>();
+            BDLog bdLog = new BDLog();
+
+
+            try
+            {
+                Resultado = bdLog.InsertLogDetalle(spName, listParametro);
+
+                Resultado = "[" + Resultado + "]";
+
+                var jsonLogDetalle = JsonConvert.DeserializeObject<LogDetalle[]>(Resultado.ToString());
+                ListLog = jsonLogDetalle.ToList();
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return ListLog;
+        }
+    }
+}
