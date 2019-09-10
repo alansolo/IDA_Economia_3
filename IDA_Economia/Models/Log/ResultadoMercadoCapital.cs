1e5458979274b24f35989243fe148446bbe5275e
Alan <Alan@DESKTOP-M949GPQ>
2019-09-09 19:03:55 -0500

    Se agrega mapeo detalle log

---
+++ IDA_Economia/Models/Log/ResultadoMercadoCapital.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDA_Economia.Models.MercadoCapital
+{
+     
+    public class ResultadoMercadoCapital
+    {
+        public List<Empresa> ListaEmpresa { get; set; }
+        public List<EncabezadoEmpresa> ListaEncabezadoEmpresa { get; set; }
+        public List<CurvaVarianza> ListaCurvaVarianza { get; set; }
+        public List<CalculoMercadoCapital> ListaCalculoMercadoCapital { get; set; }
+        public List<DatosMecadoCapital> ListaDatos { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
