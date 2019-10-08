f292314377ac0ab923bf12ef3d1f98c3124966ce
Alan <Alan@DESKTOP-M949GPQ>
2019-10-08 13:16:31 -0500

    Agregar ResultadoLog

---
+++ IDA_Economia/Models/Log/ResultadoLog.cs
@@ -0,0 +1,15 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IDA_Economia.Models.Log
+{
+     
+    public class ResultadoLog
+    {
+        public List<Entidades.Log> ListaLog { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
